340d180fe6396f3b99831f095caaf59d31214c9f
Juan Manuel <jmgonzalezgarzon@gmail.com>
2022-07-11 21:23:47 -0500

    eliminar ramas con awk

--- clean-local-branchs.sh
+++ clean-local-branchs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git fetch --all -p; git branch -vv | grep ": gone]" | awk '{ print $1 }' | xargs -r -n 1 git branch -D
+git fetch --all -p; git branch -vv | grep ": gone]" | awk '{ print "git branch -D "$1 }' | sh
