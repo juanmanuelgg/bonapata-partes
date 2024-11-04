2755f67f4d5e074013a085c95cdbdc3975167df7
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2024-11-04 04:17:31 -0500

    test fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
-export const resolve = {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-};
-export const module = {
-    rules: [
-        {
-            test: /\.(js|jsx|ts|tsx)$/,
-            exclude: /node_modules/,
-            resolve: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx']
-            },
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-react',
-                        '@babel/preset-env',
-                        '@babel/preset-flow'
-                    ]
+// eslint-disable-next-line no-undef
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx']
+                },
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react',
+                            '@babel/preset-env',
+                            '@babel/preset-flow'
+                        ]
+                    }
                 }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
             }
-        },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader', 'postcss-loader']
-        }
-    ]
+        ]
+    }
 };
