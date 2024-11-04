da98c6745cdc77974ee012e25f8f07f122d531f9
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2024-11-04 03:02:12 -0500

    fix

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        files: ["src/**/*.js"],
+        ignores: ["**/*.config.js", "!**/eslint.config.js"],
+        rules: {
+            semi: "error"
+        }
+    }
+];
