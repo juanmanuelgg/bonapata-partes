d7f84cb3be4b6a34531a7093628537ad6fb28b0c
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2024-11-04 04:25:11 -0500

    test fix

--- webpack.analyzer.config.js
+++ webpack.analyzer.config.js
@@ -1,6 +1,8 @@
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-export default {
+// eslint-disable-next-line no-undef
+module.exports = {
     // eslint-disable-next-line no-undef
     ...require('./webpack.config.js'),
     entry: './src/index.ts',
