ecccd258b350b47d52801b14f1505eca7bcadfa2
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2024-11-04 04:16:36 -0500

    test fix

--- webpack.umd.config.js
+++ webpack.umd.config.js
@@ -1,22 +1,25 @@
-export const entry = './dist/esm/index.js';
-export const output = {
-    library: 'partes',
-    libraryTarget: 'umd',
-    filename: 'umd/partes.js',
-    auxiliaryComment:
-        'Una librería libre de componentes para React, escrita en Typescript.'
-};
-export const externals = {
-    react: {
-        commonjs: 'react',
-        commonjs2: 'react',
-        amd: 'react',
-        root: 'React'
+// eslint-disable-next-line no-undef
+module.exports = {
+    entry: './dist/esm/index.js',
+    output: {
+        library: 'partes',
+        libraryTarget: 'umd',
+        filename: 'umd/partes.js',
+        auxiliaryComment:
+            'Una librería libre de componentes para React, escrita en Typescript.'
     },
-    'react-dom': {
-        commonjs: 'react-dom',
-        commonjs2: 'react-dom',
-        amd: 'react-dom',
-        root: 'ReactDOM'
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM'
+        }
     }
 };
