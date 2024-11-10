ca413f6967e2c27384940bd38596e10029821b88
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2024-11-10 09:39:33 -0500

    Arreglo manual.

--- webpack.analyzer.config.js
+++ webpack.analyzer.config.js
@@ -1,9 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-// eslint-disable-next-line no-undef
 module.exports = {
-    // eslint-disable-next-line no-undef
     ...require('./webpack.config.js'),
     entry: './src/index.ts',
     plugins: [new BundleAnalyzerPlugin()]
